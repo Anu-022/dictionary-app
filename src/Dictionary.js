11778f7e975beb331eb2fd69f4800072b6caf5f1
Anuoluwapo <ilorianuoluwapo@gmail.com>
2022-06-20 15:25:49 +0100

    created the dictionary search engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import'./Dictionary.css';
 
 export default function Dictionary() {
+    const [keyword, setKeyword] = useState("");
+    function search(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword} definition`);
+    }
+    function handleKeywordChange(event){
+       setKeyword(event.target.value);
+    }
+
     return(
         <div className='Dictionary'>
             <header className='Dictionary-header'>
-            <form>
-                    <input type="search" />
-                    <input type="submit" />
-                </form> 
-                <p> Hello</p>  
+                   <form onSubmit={search}>
+                        <input type="search" onChange={handleKeywordChange}/>
+                        <button type="submit">Search</button>
+                   </form>  
             </header>
             
         </div>
