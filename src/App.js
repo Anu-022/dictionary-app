797e171bc9f6a988d9cd6dc382cbe323ce4ce102
Anuoluwapo <ilorianuoluwapo@gmail.com>
2022-06-21 11:47:56 +0100

    added axios to the dictionary app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Dictionary from "./Dictionary";
 function App() {
   return (
     <div className="App">
-        <Dictionary />
-        <div className='footer'>Coded by Anuoluwapo Olusanya</div>
+        <header className='App-header'>
+          <h1>Dictionary App</h1>
+          <Dictionary />
+        </header>
+        <div className='footer text-center'>Coded by Anuoluwapo Olusanya</div>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import'./Dictionary.css';
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState("");
+     
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
     function search(event) {
         event.preventDefault();
         alert(`Searching for ${keyword} definition`);
+
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(handleResponse);
     }
     function handleKeywordChange(event){
        setKeyword(event.target.value);
@@ -13,13 +21,17 @@ export default function Dictionary() {
 
     return(
         <div className='Dictionary'>
-            <header className='Dictionary-header'>
-                   <form onSubmit={search}>
+            <div className='row'>
+                <div className= 'col-md-12'>
+                    <form onSubmit={search}>
                         <input type="search" onChange={handleKeywordChange}/>
                         <button type="submit">Search</button>
-                   </form>  
-            </header>
+                   </form>
+                </div>
+            </div>
+                    
+            </div>
             
-        </div>
+            
     )
 }
